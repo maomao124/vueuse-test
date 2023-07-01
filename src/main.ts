06d5f0207025bcba366297b6c5ed745919b9e0b6
mao <1296193245@qq.com>
2023-07-01 16:29:54 +0800

    useDebouncedRefHistory

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View1.vue'
+import App from './view/View2.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View2.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <h1>{{ count }}</h1>
+  <button @click="count++">+1</button>
+  <br>
+  <button @click="count--">-1</button>
+  <br>
+  <button @click="undo">undo</button>
+  <br>
+  <button @click="redo">redo</button>
+
+</template>
+
+<script setup lang="ts">
+
+import {ref} from "vue";
+import {useDebouncedRefHistory} from "@vueuse/core";
+
+const count = ref(100);
+const {history, undo, redo} = useDebouncedRefHistory(count, {deep: true, debounce: 500});
+
+</script>
+
+<style scoped>
+
+</style>
