8422c908ddbfa685b592149ee974cfabf22fec49
mao <1296193245@qq.com>
2023-07-01 16:44:09 +0800

    useStorage

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View3.vue'
+import App from './view/View4.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View4.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <h1>{{ count }}</h1>
+  <button @click="count++">+1</button>
+  <br>
+  <button @click="count--">-1</button>
+
+
+</template>
+
+<script setup lang="ts">
+
+import {onMounted, ref} from "vue";
+import {useLastChanged, useStorage} from "@vueuse/core";
+
+const count = ref(100);
+
+const storage = useStorage("count", count);
+//const storage = useStorage("count", count, sessionStorage);
+onMounted(() =>
+{
+  setInterval(() =>
+  {
+    console.log(localStorage.getItem('count'));
+  }, 1000)
+})
+
+</script>
+
+<style scoped>
+
+</style>
