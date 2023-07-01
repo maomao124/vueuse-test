7d2ccfc1326acc48a6cedadebc995784bc6ec829
mao <1296193245@qq.com>
2023-07-01 17:03:54 +0800

    useDropZone

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View5.vue'
+import App from './view/View6.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View6.vue
@@ -0,0 +1,35 @@
+<template>
+  <div ref="dropZoneRef">
+    文件放置位置
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import {useDropZone} from "@vueuse/core";
+
+const dropZoneRef = ref<HTMLDivElement>()
+
+function onDrop(files: File[] | null)
+{
+  if (files != null)
+  {
+    for (let file of files)
+    {
+      console.log(file);
+    }
+  }
+}
+
+const {isOverDropZone} = useDropZone(dropZoneRef, onDrop)
+
+</script>
+
+<style scoped>
+div {
+  background: dodgerblue;
+  width: 600px;
+  height: 600px;
+  text-align: center;
+}
+</style>
