88b9b80e9568075b6dfaec918ebafab37b32847d
mao <1296193245@qq.com>
2023-07-01 16:12:19 +0800

    光标位置

---
+++ src/view/View1.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>鼠标位置：{{ x }},{{ y }}</h1>
+</template>
+
+<script lang="ts" setup>
+import {useMouse} from "@vueuse/core";
+
+const {x, y} = useMouse();
+
+</script>
+
+<style scoped>
+
+</style>
