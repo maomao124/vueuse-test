3adb46bdbeb8a7dd076bf1cc493bbfd074671f14
mao <1296193245@qq.com>
2023-07-01 16:57:45 +0800

    useDraggable

---
+++ src/view/View5.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div ref="el" :style="style" style="position: fixed">
+      Drag me! I am at {{ x }}, {{ y }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import {ref} from "vue";
+import {useDraggable} from "@vueuse/core";
+
+const el = ref<HTMLElement | null>(null)
+const {x, y, style} = useDraggable(el, {
+  initialValue: {x: 40, y: 40},
+})
+
+
+</script>
+
+<style scoped>
+
+div {
+  background: #42b983;
+}
+</style>
