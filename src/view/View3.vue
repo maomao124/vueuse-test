9dc4c52e86eeb9dc248f84cec2a3e789228a699c
mao <1296193245@qq.com>
2023-07-01 16:34:06 +0800

    useLastChanged

---
+++ src/view/View3.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <h1>{{ count }}</h1>
+  <button @click="count++">+1</button>
+  <br>
+  <button @click="count--">-1</button>
+  <br>
+  <h1>最后一次更改时间：{{ new Date(lastChanged).toLocaleString() }}</h1>
+
+
+</template>
+
+<script setup lang="ts">
+
+import {ref} from "vue";
+import {useLastChanged} from "@vueuse/core";
+
+const count = ref(100);
+
+const lastChanged = useLastChanged(count)
+
+
+</script>
+
+<style scoped>
+
+</style>
