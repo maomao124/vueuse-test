c1183c4555a178ac883728a24fb7447a9fd1f281
mao <1296193245@qq.com>
2023-07-01 20:39:56 +0800

    useWindowScroll

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View9.vue'
+import App from './view/View10.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View10.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="div">
+    <h1>
+      {{ windowScroll.x }}<br>
+      {{ windowScroll.y }}
+    </h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import {useWindowScroll} from "@vueuse/core";
+import {watch} from "vue";
+
+const windowScroll = useWindowScroll();
+
+watch(windowScroll.x, () =>
+{
+  console.log("x更改", windowScroll.x.value, windowScroll.y.value);
+})
+watch(windowScroll.y, () =>
+{
+  console.log("y更改", windowScroll.x.value, windowScroll.y.value);
+})
+
+</script>
+
+<style scoped>
+.div {
+  text-align: center;
+  width: 500vw;
+  height: 2000vh;
+  background: dodgerblue;
+}
+
+
+</style>
