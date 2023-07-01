f63baea3dbf8398d7228fb3e5ad0dbc9c047d2f6
mao <1296193245@qq.com>
2023-07-01 19:28:37 +0800

    useMouseInElement

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View7.vue'
+import App from './view/View8.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View8.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div id="d" ref="target">
+      <h1>Hello world</h1>
+    </div>
+    <h1>{{ x }},{{ y }}<br>
+      {{ isOutside }}<br>
+      {{ elementX }},{{ elementY }}</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import {ref} from "vue";
+import {useMouseInElement} from "@vueuse/core";
+
+const target = ref(null)
+const {x, y, isOutside, elementX, elementY} = useMouseInElement(target)
+
+</script>
+
+<style scoped>
+
+#d {
+  background: darksalmon;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 200px;
+  height: 200px;
+}
+</style>
