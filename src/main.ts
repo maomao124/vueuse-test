3248b58259e2b0d4e1df70808622ddd47c4e73bc
mao <1296193245@qq.com>
2023-07-02 20:29:29 +0800

    useBrowserLocation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View13.vue'
+import App from './view/View14.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View14.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>{{ location }}</h2>
+</template>
+
+<script setup lang="ts">
+import {useBrowserLocation} from "@vueuse/core";
+import {watch} from "vue";
+
+const location = useBrowserLocation();
+console.log(location.value)
+watch(location, () =>
+{
+  console.log("监听到location更改")
+  console.log(location)
+})
+</script>
+
+<style scoped>
+
+</style>
