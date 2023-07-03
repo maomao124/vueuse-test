b934648e893d42a2bb5d47501023d453142d9bf2
mao <1296193245@qq.com>
2023-07-03 14:19:37 +0800

    useShare

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View19.vue'
+import App from './view/View20.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View20.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="toShare">点击分享</button>
+</template>
+
+<script lang="ts" setup>
+import {useShare} from "@vueuse/core";
+
+const share = useShare();
+
+function toShare(): void
+{
+  if (share.isSupported)
+  {
+    share.share({
+      title: "分享测试",
+      text: "11111111111111111111111111111111111111",
+      url: location.href
+    })
+  }
+  else
+  {
+    console.log("不支持")
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
