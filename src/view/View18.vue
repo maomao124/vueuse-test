5eeaa3680c9d068686604a6b9044955844af2557
mao <1296193245@qq.com>
2023-07-03 13:56:29 +0800

    useFullscreen

---
+++ src/view/View18.vue
@@ -0,0 +1,24 @@
+<template>
+  <img ref="img" src="public/favicon.ico" alt="图片">
+  <br>
+  <button @click="fullscreen.enter()">进入全屏</button>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import {useFullscreen} from "@vueuse/core";
+
+const img = ref<HTMLImageElement>();
+
+const fullscreen = useFullscreen(img);
+console.log(fullscreen.isSupported.value);
+
+</script>
+
+<style scoped>
+img {
+  width: 300px;
+  height: 300px;
+  background: #42b983;
+}
+</style>
