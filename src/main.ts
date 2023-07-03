dbd5841a391fd82d53997bdc13a80befc71cc0c3
mao <1296193245@qq.com>
2023-07-03 14:14:00 +0800

    useMemory

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View18.vue'
+import App from './view/View19.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View19.vue
@@ -0,0 +1,39 @@
+<template>
+  <template v-if="memory.memory">
+    <div opacity="50">
+      Used
+    </div>
+    <div>{{ size(memory.memory.usedJSHeapSize) }}</div>
+    <div opacity="50">
+      Allocated
+    </div>
+    <div>{{ size(memory.memory.totalJSHeapSize) }}</div>
+    <div opacity="50">
+      Limit
+    </div>
+    <div>{{ size(memory.memory.jsHeapSizeLimit) }}</div>
+  </template>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import {useMemory} from "@vueuse/core";
+
+const memory = useMemory();
+console.log(memory.isSupported.value)
+
+function size(v: number)
+{
+  const kb = v / 1024 / 1024
+  return `${kb.toFixed(2)} MB`
+}
+
+</script>
+
+<style scoped>
+video {
+  width: 300px;
+  height: 300px;
+  background: #42b983;
+}
+</style>
