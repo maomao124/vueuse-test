b9903b40aa691572dc1c886f3b6d7b79db26b956
mao <1296193245@qq.com>
2023-07-03 14:32:38 +0800

    useWebNotification

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View20.vue'
+import App from './view/View21.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View21.vue
@@ -0,0 +1,40 @@
+<template>
+  <button @click="()=>{if (isSupported){show()}}">通知
+  </button>
+</template>
+
+<script setup lang="ts">
+import {useWebNotification} from "@vueuse/core";
+
+const {
+  isSupported,
+  notification,
+  show,
+  close,
+  onClick,
+  onShow,
+  onError,
+  onClose,
+} = useWebNotification({
+  title: '你好，当前时间为' + new Date().toLocaleString(),
+  dir: 'auto',
+  lang: 'zh-cn',
+  renotify: true,
+  tag: '通知测试',
+})
+console.log(isSupported.value)
+onClick((e) =>
+{
+  console.log("通知被点击了,点击的通知为", e.target.title)
+})
+onClose((e) =>
+{
+  console.log("通知被关闭了 ", e.target.title)
+})
+
+
+</script>
+
+<style scoped>
+
+</style>
