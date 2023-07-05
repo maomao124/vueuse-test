9506bbd9be2e8b13aa80b8cb59fa3e6cdcbd0480
mao <1296193245@qq.com>
2023-07-05 19:40:15 +0800

    useBattery

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View22.vue'
+import App from './view/View23.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View23.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>charging: {{charging}}</h2>
+  <h2>chargingTime: {{chargingTime}}</h2>
+  <h2>dischargingTime: {{dischargingTime}}</h2>
+  <h2>level: {{level}}</h2>
+
+</template>
+
+<script lang="ts" setup>
+import {useBattery} from "@vueuse/core";
+
+const {charging, chargingTime, dischargingTime, level} = useBattery()
+
+setTimeout(()=>
+{
+  console.log(charging.value)
+  console.log(chargingTime.value)
+  console.log(dischargingTime.value)
+  console.log(level.value)
+},500)
+
+</script>
+
+<style scoped>
+
+</style>
