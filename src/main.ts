a355b559fe24390e4331e87b56f8ac2dafecaa5e
mao <1296193245@qq.com>
2023-07-05 19:51:59 +0800

    useDevicesList

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View24.vue'
+import App from './view/View25.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View25.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <h2>{{devices}}</h2>
+  <h2>{{cameras}}</h2>
+  <h2>{{microphones}}</h2>
+  <h2>{{speakers}}</h2>
+
+</div>
+</template>
+
+<script lang="ts" setup>
+import {useDevicesList} from "@vueuse/core";
+
+const {
+  devices,
+  videoInputs: cameras,
+  audioInputs: microphones,
+  audioOutputs: speakers,
+} = useDevicesList()
+
+</script>
+
+<style scoped>
+
+</style>
