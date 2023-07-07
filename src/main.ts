fb366775b9a44336a5efa989b5a4bbd7d90f867b
mao <1296193245@qq.com>
2023-07-07 14:52:46 +0800

    useTimestamp

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View35.vue'
+import App from './view/View36.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View36.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>timestamp:{{ timestamp }}</h2>
+
+</template>
+
+<script lang="ts" setup>
+
+import {useTimestamp} from "@vueuse/core";
+
+const timestamp = useTimestamp();
+
+</script>
+
+<style scoped>
+
+</style>
