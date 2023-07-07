4c6d41e6060a74acb4b20b24de5f8f3f7a113536
mao <1296193245@qq.com>
2023-07-07 14:48:51 +0800

    useNow

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View34.vue'
+import App from './view/View35.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View35.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>{{ now.toLocaleString() }}</h2>
+
+</template>
+
+<script lang="ts" setup>
+
+import {useNow} from "@vueuse/core";
+
+const {now, pause, resume} = useNow({controls: true})
+
+</script>
+
+<style scoped>
+
+</style>
