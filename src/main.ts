c74c7d06bf0e4cdb11e2740ce83bb360a707a6d5
mao <1296193245@qq.com>
2023-07-07 14:04:35 +0800

    usePageLeave

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View31.vue'
+import App from './view/View32.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View32.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>pageLeave:{{ pageLeave }}</h1>
+</template>
+
+<script lang="ts" setup>
+import {usePageLeave} from "@vueuse/core";
+
+const pageLeave = usePageLeave();
+
+</script>
+
+<style scoped>
+
+</style>
