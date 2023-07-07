d64260845d45b096d64c0285be31e1cc1294f960
mao <1296193245@qq.com>
2023-07-07 13:58:23 +0800

    useNetwork

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View30.vue'
+import App from './view/View31.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View31.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>isSupported:{{ network.isSupported }}</h1>
+  <h1>type:{{ network.type }}</h1>
+  <h1>isOnline:{{ network.isOnline }}</h1>
+  <h1>saveData:{{ network.saveData }}</h1>
+  <h1>rtt:{{ network.rtt }}</h1>
+  <h1>downlink:{{ network.downlink }}</h1>
+  <h1>downlinkMax:{{ network.downlinkMax }}</h1>
+  <h1>effectiveType:{{ network.effectiveType }}</h1>
+  <h1>offlineAt:{{ network.offlineAt }}</h1>
+  <h1>onlineAt:{{ network.onlineAt }}</h1>
+</template>
+
+<script lang="ts" setup>
+import {useNetwork} from "@vueuse/core";
+
+const network = useNetwork();
+
+</script>
+
+<style scoped>
+
+</style>
