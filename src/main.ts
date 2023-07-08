51dc2deb2eacae5c6dcb118efa3774beb321a05c
mao <1296193245@qq.com>
2023-07-08 22:17:11 +0800

    until

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View37.vue'
+import App from './view/View38.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View38.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>count:{{ count }}</h2>
+  <button @click="change">改变值</button>
+</template>
+
+<script lang="ts" setup>
+
+import {invoke, until, useCounter} from "@vueuse/core";
+import {ref} from "vue";
+
+const {count} = useCounter(10)
+
+function change()
+{
+  count.value++;
+}
+
+invoke(async () =>
+{
+  await until(count).toMatch(v => v >= 20)
+  alert('大于等于20')
+})
+
+</script>
+
+<style scoped>
+
+</style>
