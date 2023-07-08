1c8ad7285c39cdd899b9716cd23238662d8368c1
mao <1296193245@qq.com>
2023-07-08 14:11:37 +0800

    useTransition

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View36.vue'
+import App from './view/View37.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View37.vue
@@ -0,0 +1,43 @@
+<template>
+  <h2>output1:{{ output1 }}</h2>
+  <h2>output2:{{ output2 }}</h2>
+  <h2>output3:{{ output3 }}</h2>
+  <h2>output4:{{ output4 }}</h2>
+  <button @click="change">改变值</button>
+
+</template>
+
+<script lang="ts" setup>
+
+import {TransitionPresets, useTransition} from "@vueuse/core";
+import {ref} from "vue";
+
+const source = ref(0)
+
+const output1 = useTransition(source, {
+  duration: 1000,
+  transition: TransitionPresets.easeInOutCubic,
+})
+const output2 = useTransition(source, {
+  duration: 500,
+  transition: TransitionPresets.easeInOutCubic,
+})
+const output3 = useTransition(source, {
+  duration: 5000,
+  transition: TransitionPresets.easeInOutCubic,
+})
+const output4 = useTransition(source, {
+  duration: 1000,
+  transition: TransitionPresets.easeInCirc,
+})
+
+function change()
+{
+  source.value = source.value + 100
+}
+
+</script>
+
+<style scoped>
+
+</style>
