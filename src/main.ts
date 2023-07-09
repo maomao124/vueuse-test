e9315ee438a41938e442d9aaa33448205fe48948
mao <1296193245@qq.com>
2023-07-09 21:32:23 +0800

    watchOnce

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View41.vue'
+import App from './view/View42.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View42.vue
@@ -0,0 +1,38 @@
+<template>
+  <h2>count:{{ count }}</h2>
+  <h2>触发次数:{{ updateNumber }}</h2>
+  <button @click="change1">+1</button>
+  <button @click="change2">-1</button>
+</template>
+
+<script lang="ts" setup>
+
+import {watchArray, watchAtMost, watchDebounced, watchOnce} from "@vueuse/core";
+import {ref} from "vue";
+
+const count = ref(100)
+
+const updateNumber = ref(0);
+
+function change1()
+{
+  count.value++
+}
+
+function change2()
+{
+  count.value--;
+}
+
+watchOnce(count, () =>
+{
+  console.log(count.value)
+  updateNumber.value++;
+})
+
+
+</script>
+
+<style scoped>
+
+</style>
