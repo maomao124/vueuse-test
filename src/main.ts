cd19d8fd45c66c27120455eba4a597c864fadef3
mao <1296193245@qq.com>
2023-07-10 16:41:17 +0800

    useAsyncQueue

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View47.vue'
+import App from './view/View48.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View48.vue
@@ -0,0 +1,42 @@
+<template>
+</template>
+
+<script lang="ts" setup>
+
+import {get, set, useAsyncQueue} from "@vueuse/core";
+import {ref} from "vue";
+
+const p1 = (() =>
+{
+  return new Promise((resolve) =>
+  {
+    setTimeout(() =>
+    {
+      resolve(1000)
+    }, 10)
+  })
+})
+
+const p2 = ((result: number) =>
+{
+  return new Promise((resolve) =>
+  {
+    setTimeout(() =>
+    {
+      resolve(1000 + result)
+    }, 20)
+  })
+})
+
+const {activeIndex, result} = useAsyncQueue([p1, p2])
+
+console.log(activeIndex.value)
+
+console.log(result)
+</script>
+
+<style scoped>
+h2 {
+  color: red;
+}
+</style>
