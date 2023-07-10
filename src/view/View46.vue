0c513f7080c1c419ac5f0e6d3d6dbc58be689849
mao <1296193245@qq.com>
2023-07-10 16:32:12 +0800

    get

---
+++ src/view/View46.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>count:{{ count }}</h2>
+</template>
+
+<script lang="ts" setup>
+
+import {get} from "@vueuse/core";
+import {ref} from "vue";
+
+const count = ref(200);
+
+console.log(get(count))
+</script>
+
+<style scoped>
+h2 {
+  color: red;
+}
+</style>
