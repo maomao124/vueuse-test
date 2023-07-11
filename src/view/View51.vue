33a7f03bf89e13ec4b2a1e7381eff55e95d3adf5
mao <1296193245@qq.com>
2023-07-11 11:20:31 +0800

    usePrevious

---
+++ src/view/View51.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>count:{{ count }}</h2>
+  <h2>上一次的值:{{ previous }}</h2>
+  <button @click="change1">+1</button>
+  <button @click="change2">-1</button>
+</template>
+
+<script lang="ts" setup>
+
+import {usePrevious} from "@vueuse/core";
+import {ref} from "vue";
+
+const count = ref(100)
+const previous = usePrevious(count);
+
+function change1()
+{
+  count.value++
+}
+
+function change2()
+{
+  count.value--;
+}
+
+</script>
+
+<style scoped>
+
+</style>
