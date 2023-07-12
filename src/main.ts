43335e426c8f2a276da4f9b08e5285c45c72d36d
mao <1296193245@qq.com>
2023-07-12 19:08:33 +0800

    useAxios

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './view/View51.vue'
+import App from './view/View52.vue'
 
 createApp(App).mount('#app')
---
+++ src/view/View52.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>data1:{{ data1 }}</h2>
+  <h2>data2:{{ data2 }}</h2>
+</template>
+
+<script lang="ts" setup>
+
+import {useAxios} from '@vueuse/integrations/useAxios'
+import axios from "axios";
+
+const {data1, isFinished} = useAxios('/api/posts')
+
+const instance = axios.create({
+  baseURL: '/api',
+})
+
+const {data2} = useAxios('/posts', {method: 'POST'}, instance)
+
+</script>
+
+<style scoped>
+
+</style>
